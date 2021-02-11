c55fc994500f0bcfa58da1e46cf66b59d83f1c22
gayfulina <irinmdr@gmail.com>
2021-02-11 14:20:55 -0800

    mobileNav

--- shared.js
+++ shared.js
@@ -2,6 +2,8 @@ const backdrop = document.querySelector('.backdrop');
 const modal = document.querySelector('.modal');
 const selectPlanButtons = document.querySelectorAll('.plan button');
 const modalNoButton = document.querySelector('.modal__action--negative');
+const toggleButton = document.querySelector('.toggle-button')
+const mobileNav = document.querySelector('.mobile-nav')
 
 
 for (let i = 0; i < selectPlanButtons.length; i++) {
